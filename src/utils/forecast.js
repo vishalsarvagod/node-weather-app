45ab9c8d95646fd89b70b536e2b532e5666ba1cb
Vishal Sarvagod <vishalsarvagod91@gmail.com>
2021-04-02 20:54:25 -0400

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined,
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike +
+                 ' degree out. The humidity is '+ body.current.humidity + "%.")
         }
     })
 }
